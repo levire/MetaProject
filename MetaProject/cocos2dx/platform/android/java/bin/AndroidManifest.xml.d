9e783ed02f26d0207303226f3f2614c2cc01555b
Daniel Kiedrowski <daniel.kiedrowski@googlemail.com>
2013-01-04 23:07:04 +0100

    + REMOVED generated and compiled stuff

--- MetaProject/cocos2dx/platform/android/java/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/Wolf/Documents/levire/Metaproject/cocos2dx/platform/android/java/bin/AndroidManifest.xml : \
-/Users/Wolf/Documents/levire/Metaproject/cocos2dx/platform/android/java/AndroidManifest.xml \
--- MetaProject/cocos2dx/platform/ouya/java/gen/R.java.d
+++
@@ -1,1 +0,0 @@
- : /Users/Wolf/Documents/levire/Metaproject/cocos2dx/platform/android/java/bin/AndroidManifest.xml \
