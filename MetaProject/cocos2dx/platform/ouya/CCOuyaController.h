50498073b631475b46f41bd9b04644ab9f215647
Daniel Kiedrowski <daniel.kiedrowski@googlemail.com>
2013-01-07 17:32:08 +0100

    + ADDED listeners to the CCOuyaController, which are informed about every event from the controllers

--- MetaProject/cocos2dx/platform/ouya/CCOuyaController.h
+++ MetaProject/cocos2dx/platform/ouya/CCOuyaController.h
@@ -2,16 +2,31 @@
 #define __CC_OUYA_CONTROLLER_H__
 
 #include <map>
+#include <vector>
 #include <jni.h>
 #include "cocos2d.h"
 
 NS_CC_BEGIN
+
+class CC_DLL CCOuyaController;
+class IOuyaControllerListener
+{
+public:
+	virtual void onControllerKeyDown(int keyCode, CCOuyaController* controller) =0;
+	virtual void onControllerKeyUp(int keyCode, CCOuyaController* controller) =0;
+};
 
 class CC_DLL CCOuyaController
 {
 public:
 
 	static CCOuyaController* controllerByDeviceId(int deviceId);
+	static void onKeyDown(int keyCode, int deviceId);
+	static void onKeyUp(int keyCode, int deviceId);
+
+	static void addListener(IOuyaControllerListener* listener);
+	static void removeListener(IOuyaControllerListener* listener);
+	static bool registered(IOuyaControllerListener* listener);
 
 private:
 
@@ -25,6 +40,9 @@ private:
 
 	typedef std::map<int, CCOuyaController*> Controllers;
 	static Controllers controllers;
+
+	typedef std::vector<IOuyaControllerListener*> Listeners;
+	static Listeners listeners;	
 };
 
 NS_CC_END //__CC_OUYA_CONTROLLER_H__
