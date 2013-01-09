1c2fb8653591dfd1ad31d9037373aa1f53d23a45
Daniel Kiedrowski <daniel.kiedrowski@googlemail.com>
2013-01-09 22:58:27 +0100

    Added ver basic example of using gtest

---
+++ MetaProject/project/Tests/SampleTest.cpp
@@ -0,0 +1,20 @@
+//
+//  SampleTest.cpp
+//  LevireTemplate
+//
+//  Created by Daniel Kiedrowski on 09.01.13.
+//
+//  Provides a basic example of a unit test using gtest
+
+#include <gtest.h>
+
+TEST(Example, Success)
+{
+    EXPECT_TRUE(true);
+}
+
+TEST(Example, Failure)
+{
+    EXPECT_TRUE(false);
+}
+
