c7c0ecf7df91fb5381c4885ec0c625f17217f58d
Daniel Kiedrowski <daniel.kiedrowski@googlemail.com>
2013-01-15 18:02:21 +0100

    Added different source files for building the app or the associated tests, which needs a test runner

--- MetaProject/project/proj.ouya/jni/Android.mk
+++ MetaProject/project/proj.ouya/jni/Android.mk
@@ -8,9 +8,22 @@ LOCAL_MODULE_FILENAME := libhellocpp
 
 LOCAL_CFLAGS +=  -DOUYA=1 -DCOCOS2D_DEBUG=1
 
-LOCAL_SRC_FILES := hellocpp/main.cpp \
-                   ../../Classes/AppDelegate.cpp \
-                   ../../Classes/HelloWorldScene.cpp
+NORMAL_SRC_FILES := \
+    hellocpp/main.cpp \
+    ../../Classes/AppDelegate.cpp \
+    ../../Classes/HelloWorldScene.cpp
+
+TEST_SRC_FILES := \
+    hellocpp/main.cpp \
+    ../../Tests/AppDelegate.cpp \
+    ../../Tests/TestRunnerScene.cpp \
+    ../../Tests/SampleTest.cpp
+
+ifeq ($(BUILD_TESTS), true)
+    LOCAL_SRC_FILES := $(TEST_SRC_FILES)
+else    
+    LOCAL_SRC_FILES := $(NORMAL_SRC_FILES)
+endif                  
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes
 
@@ -18,7 +31,7 @@ LOCAL_WHOLE_STATIC_LIBRARIES := cocos2dx_static
 LOCAL_WHOLE_STATIC_LIBRARIES += cocos_extension_static
 
 ifeq ($(BUILD_TESTS), true)
-	LOCAL_WHOLE_STATIC_LIBRARIES += gtest_static
+    LOCAL_WHOLE_STATIC_LIBRARIES += gtest_static
 endif
 
 include $(BUILD_SHARED_LIBRARY)
