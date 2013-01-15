a98943f8635df8f4e75082e5e1705e27b09939b1
Daniel Kiedrowski <daniel.kiedrowski@googlemail.com>
2013-01-15 17:27:40 +0100

    Removed some files from the Android.mk of the gtest library because they are not for the library but for some convenience or test scripts

--- MetaProject/gtest/Android.mk
+++ MetaProject/gtest/Android.mk
@@ -2,24 +2,15 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := gtest
+LOCAL_MODULE := gtest_static
 
 LOCAL_MODULE_FILENAME := libgtest
 
-LOCAL_SRC_FILES := \
-    src/gtest_main.cc \
-    src/gtest-all.cc \
-    src/gtest-death-test.cc \
-    src/gtest-filepath.cc \
-    src/gtest-internal-inl.h \
-    src/gtest-port.cc \
-    src/gtest-printers.cc \
-    src/gtest-test-part.cc \
-    src/gtest-typed-test.cc \
-    src/gtest.cc
+LOCAL_SRC_FILES := src/gtest-all.cc
 
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
 
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/include/gtest/internal
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/include/gtest/internal \
+                    $(LOCAL_PATH)/include
                 
 include $(BUILD_STATIC_LIBRARY)
