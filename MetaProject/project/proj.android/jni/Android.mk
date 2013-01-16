08d7ffea035fcd5d111cc56b2ed7c9628379623d
Daniel Kiedrowski <daniel.kiedrowski@googlemail.com>
2013-01-16 21:28:45 +0100

    Added the unit test building and extraction of the LOCAL_SRC_FILES to the android target Android.mk

--- MetaProject/project/proj.android/jni/Android.mk
+++ MetaProject/project/proj.android/jni/Android.mk
@@ -6,17 +6,30 @@ LOCAL_MODULE := hellocpp_shared
 
 LOCAL_MODULE_FILENAME := libhellocpp
 
-LOCAL_SRC_FILES := hellocpp/main.cpp \
-                   ../../Classes/AppDelegate.cpp \
-                   ../../Classes/HelloWorldScene.cpp
+LOCAL_CFLAGS += -DCOCOS2D_DEBUG=1
+
+ifeq ($(BUILD_TESTS), true)
+    include $(LOCAL_PATH)/../../Tests/Android.mk
+else
+    include $(LOCAL_PATH)/../../Classes/Android.mk    
+endif
+
+LOCAL_SRC_FILES += hellocpp/main.cpp
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes
 
 LOCAL_WHOLE_STATIC_LIBRARIES := cocos2dx_static
 LOCAL_WHOLE_STATIC_LIBRARIES += cocos_extension_static
 
+ifeq ($(BUILD_TESTS), true)
+    LOCAL_WHOLE_STATIC_LIBRARIES += gtest_static
+endif
+
 include $(BUILD_SHARED_LIBRARY)
 
 $(call import-module,cocos2dx/platform/android)
 $(call import-module,extensions/android)
 
+ifeq ($(BUILD_TESTS), true)
+    $(call import-module,gtest)
+endif
