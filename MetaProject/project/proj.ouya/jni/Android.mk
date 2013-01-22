fad76b7b1b00a0fa9fc482dd94d9706d1a42c5f5
Daniel Kiedrowski <daniel.kiedrowski@googlemail.com>
2013-01-22 11:25:50 +0100

    Added linking of Box2D and chipmunk to Android.mk for the OUYA target

--- MetaProject/project/proj.ouya/jni/Android.mk
+++ MetaProject/project/proj.ouya/jni/Android.mk
@@ -20,6 +20,8 @@ LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes
 
 LOCAL_WHOLE_STATIC_LIBRARIES := cocos2dx_static
 LOCAL_WHOLE_STATIC_LIBRARIES += cocos_extension_static
+LOCAL_WHOLE_STATIC_LIBRARIES += box2d_static
+LOCAL_WHOLE_STATIC_LIBRARIES += chipmunk_static
 
 ifeq ($(BUILD_TESTS), true)
     LOCAL_WHOLE_STATIC_LIBRARIES += gmock_static
@@ -30,6 +32,8 @@ include $(BUILD_SHARED_LIBRARY)
 
 $(call import-module,cocos2dx/platform/ouya)
 $(call import-module,extensions/ouya)
+$(call import-module,external/Box2D)
+$(call import-module,external/chipmunk)
 
 ifeq ($(BUILD_TESTS), true)
     $(call import-module,gmock/)
