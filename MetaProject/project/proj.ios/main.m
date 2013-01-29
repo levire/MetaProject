8cc41f324d4feed35222bd38d3b46c338461f21e
Daniel Kiedrowski <daniel.kiedrowski@googlemail.com>
2013-01-29 14:30:29 +0100

    Added gconv to the iOS target (work-in-progress)

--- MetaProject/project/proj.ios/main.m
+++ MetaProject/project/proj.ios/main.m
@@ -10,6 +10,13 @@
 
 int main(int argc, char *argv[]) {
     
+    const char *prefix = "GCOV_PREFIX";
+    const char *prefixValue = [[NSHomeDirectory() stringByAppendingPathComponent:@"Documents"] cStringUsingEncoding:NSASCIIStringEncoding]; // This gets the filepath to the app's Documents directory
+    const char *prefixStrip = "GCOV_PREFIX_STRIP";
+    const char *prefixStripValue = "1";
+    setenv(prefix, prefixValue, 1); // This sets an environment variable which tells gcov where to put the .gcda files.
+    setenv(prefixStrip, prefixStripValue, 1); // This tells gcov to strip the default prefix, and use the filepath that we just declared.
+    
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
     int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
     [pool release];
